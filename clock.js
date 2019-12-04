ffbbeda5335645e477e129c9d0e407074b016b69
baelanche <baelanche@naver.com>
2019-12-04 16:33:23 +0900

    create clock

--- clock.js
+++ clock.js
@@ -1,7 +1,15 @@
-const clockContainer = document.querySelector(".js-clock");
+const clockContainer = document.querySelector(".js-clock"),
+    clockTitle = clockContainer.querySelector("h1");
+
+function getTime() {
+    const date = new Date();
+    const hours = date.getHours();
+    const minutes = date.getMinutes();
+    clockTitle.innerText = `${hours}:${minutes}`;
+}
 
 function init() {
-
+    getTime();
 }
 
 init();
